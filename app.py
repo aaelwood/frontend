d501ed00c5df8b40d5a87b170f70a16763b90d65
Alexis Elwood <aaelwood@uiowa.edu>
2025-04-29 16:01:19 -0500

    Update app.py - API url

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config["SECRET_KEY"] = "seasdad(*2sffcra01^23sdet"
 CORS(app)
 
 # Get this URL from the Azure Overview page of your API web app
-api_url = "aaelwood-api-f4eggnfpb4fkd5hk.eastus-01.azurewebsites.net"  # base url for API endpoints
+api_url = "https://aaelwood-api-f4eggnfpb4fkd5hk.eastus-01.azurewebsites.net"  # base url for API endpoints
 
 
 # main index page route
